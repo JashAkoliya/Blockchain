20b7278ccbece19621759eba0b3270afdd432eae
JashAkoliya <100468694+JashAkoliya@users.noreply.github.com>
2023-06-21 17:49:27 +0530

    SetRandom.js

--- Ass8/SetRandom.js
+++ Ass8/SetRandom.js
@@ -2,8 +2,10 @@ let s = new Set();
 for(let i=0;i<4;i++){
     s.add(parseInt(Math.random()*10));
 }
+console.log("values:- ");
 s.forEach((k)=>console.log(k));
 
+console.log("Check that contain 8:- ");
 if(s.has(8)){
     console.log("true");
 }
