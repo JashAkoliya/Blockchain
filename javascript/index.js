8ba189a42016b0dc0ddddbdcc5b6ac3d3d9e29a8
Akoliya Jash Vijaybhai <100468694+JashAkoliya@users.noreply.github.com>
2023-07-10 05:49:56 +0530

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,119 @@
+// "use strict"  // we are aking js to behave strictly
+// console.log("Hello jash")
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(5,3))
+// console.log("hii")
+
+// let num=9;
+// num=10;
+// let str = "Jash"
+// str = "jay"
+// console.log(str)
+// console.log(num)
+
+// let  n=4
+// n='jash'
+// n=5
+// console.log(n)
+// console.log(n, typeof n)
+
+// let y
+// console.log(y, typeof y)
+
+// let user=String(123) //Number('123')
+// console.log(typeof user)
+
+// NaN - not a number (illegal number)
+// let s = Number('jash 123')
+// let s = parseInt('123 jash')  // it will forcely typecast
+// console.log(s, typeof s)
+
+// let n1 = 8
+// let n2 = ++n1
+// console.log(n1,n2)
+
+// let num1 = 5
+// let num2 = "5"
+// let result = num1 === num2  
+// // result = num1 < num2
+// console.log(result)
+
+// == compare the value not type
+// === compare the value and type as well
+
+// let u1 = "Jash"
+// let u2 = "jay"
+// let r = u1+" "+u2
+// console.log(r)
+
+// let s1 = 5
+// let s2 = 10
+// let res = s1+s2;
+// console.log(`the addition of ${s1} and ${s2} is ${res}`)
+
+// let nums = 2;
+// let res = nums%2===0 ? "even" : "odd"
+// console.log(res)
+
+// let i=5
+// while(i>0){
+//     console.log('jash')
+//     i--
+// }
+
+
+// for (let j = 0; j < 2; j++) {
+//   for (let i = 0; i < 5; i++) {
+//     if(i===2)
+//         break
+//     // continue
+//     console.log('Hello' + i)
+//   }
+//   console.log('the end')
+// }
+
+
+// let day = 'sat'
+// switch (day) {
+//     case "sun":
+//     case "sat":
+//         console.log('8am');
+//         break
+
+//     case "mon":
+//     case"tue":
+//     case"thr":
+//     case"fri":
+//         console.log('5am');
+//         break
+
+//     case "wed":
+//         console.log('6am');
+//         break
+
+//     default:
+//         console.log("exit");
+//         break;
+// }
+
+
+
+// ----------------------
+// function
+
+// let n1 = 5;
+// let n2 = 3;
+// let n3 = 2;
+// function add(s1,s2,s3=0){
+//     return s1 + s2 + s3;
+// }
+// let result = add(n1,n2,n3)
+// console.log(result);
+
+
+// let demo = add(4,6,1)
+// console.log(demo);
